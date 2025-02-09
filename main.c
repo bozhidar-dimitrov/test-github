24e9bb4647d7c99a578a2dfd27cf90ce0cfa537b
Bozhidar Dimitrov <bozhidar.dimitrov@gmail.com>
2025-02-09 19:09:12 +0200

    Creating new student

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include "student.h"
 
 int main(void) {
 
   printf("!Hello, world!");
   printf("This is modifying the file\n");
+
+  struct Student student;
 
   return 0;
 }
